9510060e6007203c6d9da620f75f002158c954fc
DURANDBoris <64001116+DURANDBoris@users.noreply.github.com>
2020-05-18 10:58:40 +0200

    test2

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
-let A = 42;
-A = String(A);
-console.log(typeof(A));
+let couleur = /*mettez une couleur ici*/"bleu";
+if (couleur === "bleu") {
+console.log("La couleur est bleu");
+} else if (couleur === "rouge") {
+console.log("La couleur est rouge");
+} else if (couleur === "vert") {
+console.log("La couleur est vert");
+} else if (couleur === "jaune") {
+console.log("La couleur est jaune");
+} else {
+console.log(`La couleur était: ${couleur}`);
+}
