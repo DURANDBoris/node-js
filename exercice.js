2749cb71dd837640e46ee98bace6e505ca355d75
DURANDBoris <64001116+DURANDBoris@users.noreply.github.com>
2020-05-18 11:16:51 +0200

    Exercice ages

---
+++ exercice.js
@@ -0,0 +1,13 @@
+let A = -3;
+if (A >= 18 && A <= 100)
+{
+    console.log("vous etes Majeur");
+}
+else if(A >= 0 && A < 18)
+{
+    console.log("Vous etes mineur");
+}
+else
+{
+    console.log("Vous vous moquez de moi ?");
+}
